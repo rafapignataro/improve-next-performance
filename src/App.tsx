0868a79e6e8b5aa7cdc9af8e3987ced172cad20b
Rafael Pignataro <rafael.pignataro@singu.com.br>
2021-08-30 11:06:22 -0300

    Improved otimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
@@ -35,13 +35,13 @@ export function App() {
   const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
-  useEffect(() => {
+  useMemo(() => {
     api.get<GenreResponseProps[]>('genres').then(response => {
       setGenres(response.data);
     });
   }, []);
 
-  useEffect(() => {
+  useMemo(() => {
     api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
       setMovies(response.data);
     });
@@ -51,9 +51,9 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  }, [])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,5 @@
+import { List, ListRowRenderer } from 'react-virtualized';
 import { MovieCard } from "./MovieCard";
-
 interface ContentProps {
   selectedGenre: {
     id: number;
@@ -20,18 +20,34 @@ interface ContentProps {
 }
 
 export function Content({ selectedGenre, movies }: ContentProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style} >
+        <MovieCard
+          key={movies[index].imdbID}
+          title={movies[index].Title}
+          poster={movies[index].Poster}
+          runtime={movies[index].Runtime}
+          rating={movies[index].Ratings[0].Value}
+        />
+      </div>
+    )
+  }
+  
   return (
     <div className="container">
       <header>
         <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
       </header>
-
       <main>
-        <div className="movies-list">
-          {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-          ))}
-        </div>
+        <List
+          height={550}
+          rowHeight={440}
+          width={800}
+          overscanRowCount={1}
+          rowCount={movies.length}
+          rowRenderer={rowRenderer}
+          />
       </main>
     </div>
   )
